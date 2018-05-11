64b06fa37cd1bb04da6fe76964aa84d4f901649d
Carlos Hijano <carloshijano@msn.com>
2018-05-11 12:47:38 +0200

    desarrollo de funciones hecho Coincidencias

--- alacena.py
+++ alacena.py
@@ -2,11 +2,57 @@ from flask import Flask
 from flask import render_template,request,redirect,session
 import json
 
+##########################################
+#         VARIABLES GLOBALES             #
+##########################################
 
+URL="https://test-es.edamam.com/search"
+RECETAS=[]
+NUM=20 		# Número de recetas solicitadas a la API
+PAG=10		# Número de recetas por página
+
+##########################################
+#              FUNCIONES                 #
+##########################################
+
+def SolicitarRecetas (ingrediente):
+	HEAD = {
+		'app_id':os.environ["recipe_id"],
+		'app_key': os.environ["recipe_key"], 
+		'q' : ingrediente,
+		'to' : NUM
+		}
+	c=requests.get(URL ,params=HEAD)
+	
+	if c.status_code == 200:
+		todo=c.json()
+		for re in todo["hits"]:
+			dic={}
+			dic["nombre"]=re["recipe"]["label"]
+			dic["imagen"]=re["recipe"]["image"]
+			dic["url"]=re["recipe"]["url"]
+			dic["calorias"]=re["recipe"]["calories"]
+			dic["ingredientes"]=re["recipe"]["ingredientLines"]
+			dic["coin"]=0
+			if 'Vegan' in re["recipe"]["healthLabels"]:	
+				dic["vegan"]=True
+			else:
+				dic["vegan"]=False	
+			RECETAS.append(dic)
+	else:
+		print("API no disponible...")
+
+def	Coincidencias (ingre):
+	for receta in RECETAS:
+		for ing in ingre.strip().split(","):
+			for x in receta["ingredientes"]:
+				if ing.upper() in x.upper():
+					receta["coin"]+=1
 
 ###########################################
-                 MAIN
+#                 MAIN                    #
 ###########################################
+
 @app.route('/')
 def inicio():
 	return render_template("index.html")
--- pruebaAPI.py
+++ pruebaAPI.py
@@ -4,7 +4,9 @@ import os
 
 #### VARIABLES GLOBALES ####
 URL="https://test-es.edamam.com/search"
-
+RECETAS=[]
+NUM=20 		# Número de recetas solicitadas a la API
+PAG=10		# Número de recetas por página
 
 #### FUNCIONES ####
 
@@ -13,10 +15,10 @@ def SolicitarRecetas (ingrediente):
 		'app_id':os.environ["recipe_id"],
 		'app_key': os.environ["recipe_key"], 
 		'q' : ingrediente,
-		'to' : 2
+		'to' : NUM
 		}
 	c=requests.get(URL ,params=HEAD)
-	recetas=[]
+	
 	if c.status_code == 200:
 		todo=c.json()
 		for re in todo["hits"]:
@@ -31,40 +33,35 @@ def SolicitarRecetas (ingrediente):
 				dic["vegan"]=True
 			else:
 				dic["vegan"]=False	
-			recetas.append(dic)
-		return recetas		
+			RECETAS.append(dic)
 	else:
 		print("API no disponible...")
 
+def	Coincidencias (ingre):
+	for receta in RECETAS:
+		for ing in ingre.strip().split(","):
+			for x in receta["ingredientes"]:
+				if ing.upper() in x.upper():
+					receta["coin"]+=1			
 
-#def	Coincidencias (recetas,ingredientes):
-#	for receta in recetas:
-#		for ing in ingredientes:
-#			if ing in receta["ingredientes"]:
-#				receta[""]
-
-
-
-#def Enviar ():
-
-
-
-# sorted(fijos,key=lambda x: x[4])
-
-def ImprimirConsola (recetas):
-	for receta in recetas:
+def ImprimirConsola ():
+	print("######## RECETAS ########")
+	for receta in RECETAS:
 		print("\n    @ %s"%receta["nombre"])
 		print("        > Imagen: %s"%receta["imagen"])
 		print("        > Url: %s"%receta["url"])
 		print("        > Calorias: %f kcal"%receta["calorias"])
 		print("        > Vegano:",receta["vegan"])
+		print("        > Coincidencias: %i"%receta["coin"])
 		print("        > Ingredientes:")
 		for ing in receta["ingredientes"]:
 			print("            * %s"%ing)
 
+############ MAIN ##############
 
 ingredientes=input("Introduce los ingredientes separados por comas: ")
-print("######## RECETAS ########")
 for ing in ingredientes.strip().split(","):
-	recetas=SolicitarRecetas(ing)
-	ImprimirConsola(recetas)
+	SolicitarRecetas(ing)
+	Coincidencias(ingredientes)
+sorted(RECETAS,key=lambda x: x["coin"])
+ImprimirConsola()
